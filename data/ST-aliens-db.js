a8f01e301de4fed5101b90cbe253a916447d3ff1
jg932 <jared.h.goldstein@gmail.com>
2021-10-29 16:05:16 -0400

    part 2

--- data/ST-aliens-db.js
+++ data/ST-aliens-db.js
@@ -2,6 +2,9 @@ const aliens = [
   {text: 'Klingons', evil: false, _id: 125223},
   {text: 'Borg', evil: true, _id: 127904},
   {text: 'Betazoid', evil: false, _id: 139608},
+  {text: 'Romulans', evil: true, _id: 149608},
+  {text: 'Cardassians', evil: true, _id: 129608},
+  {text: 'Pakleds', evil: false, _id: 179608},
 ]
 
 const find = (conditions, callback) => {
